59a4e9715187f2f9f9b5c1fb6fe4bfe5e6c73332
Avigail <6786553@gmail.com>
2023-07-05 11:50:30 +0300

    finish storyBook

--- ims-client/src/Stories/Page.stories.ts
+++ ims-client/src/Stories/Page.stories.ts
@@ -18,7 +18,7 @@ export const LoggedOut: Story = {};
 
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
 export const LoggedIn: Story = {
-  play: async ({ canvasElement }) => {
+  play: async ({ canvasElement}) => {
     const canvas = within(canvasElement);
     const loginButton = await canvas.getByRole('button', {
       name: /Log in/i,
