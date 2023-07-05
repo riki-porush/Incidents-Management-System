57b1a38e9210a5485e7ae9df2279f8bbb88f74d1
Batzion <Batziongreen3320@gmail.com>
2023-07-05 16:48:15 +0300

    create theme

--- ims-client/src/App.tsx
+++ ims-client/src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider } from '@emotion/react';
+import theme from './theme';
+import { Button, colors } from '@mui/material';
 
-function App() {
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <ThemeProvider theme={theme}>
 
+<Button  variant="outlined" color='success' >Outlined</Button>
+<Button variant="contained" style={{ backgroundColor: theme.palette.secondary.main }}>
+          Custom Color Button
+          </Button>
+          
+     </ThemeProvider>
+  );
+};
 export default App;
+
+
+
+
+
+
