77c5c93817b344cead2afc81e26fda4d7b05e527
riki mitelman <rikimitelman@gmail.com>
2023-07-05 18:33:38 +0300

    incident

--- ims-server/src/app.ts
+++ ims-server/src/app.ts
@@ -3,23 +3,18 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import config from './config/config'
 import mongoose from 'mongoose'
-import userRout from './routes/userRout'
+import incidentRout from './routes/IncidentRout'
 
-/**
- * this is just a basic request/response
- *
- */
+
 const app = express()
-// Middleware
 app.use(cors())
 app.use(bodyParser.json())
-app.use('/users', userRout)
+app.use('/incident', incidentRout)
+
 mongoose
   .connect(config.mongo.url)
   .then(() => {
     console.info('Connected to mongoDB.')
-    // Start the server
-    // note: if you run port=5000
     const port = config.server.port
     app.listen(port, () => {
       console.log(`Server is listening on port ${port}`)
