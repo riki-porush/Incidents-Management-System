c81e8ca1df0ce84cdea097ab5714a73e86b05c81
Batzion <Batziongreen3320@gmail.com>
2023-07-06 14:24:15 +0300

    update to green

--- ims-client/src/App.tsx
+++ ims-client/src/App.tsx
@@ -12,7 +12,7 @@ const store = configureStore()
 function App() {
   return (
     <div className="App">
-         <ThemeProvider theme={theme}>
+          <ThemeProvider theme={theme}>
       <Provider store={store}>
         <LeftDrawer></LeftDrawer>
       </Provider>
