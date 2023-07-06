94522d2d4270208a63d50a2beb4166c2ba0a8f86
YaelKofman <yaelk1204@gmail.com>
2023-07-06 15:16:11 +0300

    [IMS-136] finish

--- ims-client/src/components/datePicker.tsx
+++ ims-client/src/components/datePicker.tsx
@@ -12,6 +12,7 @@ interface getAllProps {
 export function DatePickerComp({ date, setDate }: getAllProps) {
     // החלטה שרירותית שיוכלו לעדכן על תאריך החל משנה שעברה ועד היום....אם נראה לכן שיש צורך לשנות אז יאללהה
     const today = new Date()
+    //
     const yearAgo = new Date()
     yearAgo.setFullYear(today.getFullYear() - 1)
     const selectDateHandler = (d: Date) => {
