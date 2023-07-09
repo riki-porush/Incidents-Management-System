4a67927a2e9915d535281a2c12e08f3d825d1ee9
Batzion <Batziongreen3320@gmail.com>
2023-07-09 17:06:35 +0300

    theme finaly

--- ims-client/src/theme.ts
+++ ims-client/src/theme.ts
@@ -1,65 +1,78 @@
 import { colors } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
-
 const theme = createTheme({
     palette: {
         primary: {
-        
-           light: '#2F854F1A',
-          main: '#2F854F',
-         dark: '#2F854F',
-          contrastText: '#FFFFFF',
+            light: '#2F854F1A', //light-green
+            main: '#000000', //black
+            dark: '#000000',
+            contrastText: '#FFFFFF',
         },
         secondary: {
-          //gray
-            light: '#D9D9D9',
-            main: '#D9D9D9',
+            light: '#2F854F1A', //light-green
+            main: '#2F854F', //green
             dark: '#D9D9D9',
             contrastText: '#FFFFFF',
-          },
-          success: {
+        },
+        success: {
             light: '#4CAF50',
             main: '#2E7D32',
             dark: '#1B5E20',
             contrastText:'#fff'
-          },
-          error: {
+        },
+        error: {
             light: '#EF5350',
             dark: '#C62828',
             main: '#D32F2F',
             contrastText:'#fff'
-          },
-          warning:{
+        },
+        warning:{
             light: '#FF9800',
             dark: '#E65100',
             main: '#ED6C02',
             contrastText:'#fff'
-          },
-      //white
-          info: {
-            light: '#000000',
-            dark: '#000000',
-            main: '#000000',
-            contrastText:'#000000',
-          },
         },
-    typography: {
-        fontFamily: 'Poppins',
-        fontSize: 15,
-        // fontWeight: 500,
-        //  lineHeight: 23,
-        //  letterSpacing: 0,
+        info: {
+            light: '#D9D9D9',
+            dark: '#D9D9D9',
+            main: '#D9D9D9', //gray
+            contrastText:'#FFFFFF',
+        },
+      },
+      typography: {
+          fontFamily: 'Poppins',
+          fontSize: 15,
       },
       components: {
-        MuiButton: {
-          styleOverrides: {
-            root: {
-              backgroundColor:'#2F854F',
-              color:'#FFFFFF',//לבן
-              border:'#FFFFFF',
+          MuiButton: {
+            styleOverrides: {
+              root: {
+                backgroundColor:'#2F854F',
+                color:'#FFFFFF', //white
+                border:'#FFFFFF',
+              }
             }
-          }
-        },
+          },
     }
 })
-export default theme
+export default theme;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
