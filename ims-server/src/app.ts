c7a20f1bd453b998d8223434a22718f661071866
ShiraVaknin <117908802+ShiraVaknin@users.noreply.github.com>
2023-07-10 16:03:10 +0300

    Revert "Ims 44"

--- ims-server/src/app.ts
+++ ims-server/src/app.ts
@@ -2,20 +2,38 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import express from 'express'
 import mongoose from 'mongoose'
+import swaggerJSDoc from 'swagger-jsdoc'
 import swaggerUI from 'swagger-ui-express'
 
-import fs from 'fs'
-import config from './config/config' 
+import config from './config/config'
 import incidentRout from './routes/IncidentRout'
 
-const swaggerFile: any = (process.cwd() + '/src/Swagger.json')
-const swaggerData: any = fs.readFileSync(swaggerFile, 'utf8')
-const swaggerDocument = JSON.parse(swaggerData)
-swaggerDocument.servers[0].url = `http://localhost:${config.server.port}`
+const swaggerOptions: swaggerJSDoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Your API',
+      version: '1.0.0',
+      description: 'API documentation using Swagger',
+    },
+    servers: [
+      {
+        url: `http://localhost:${config.server.port}`, // Replace with your server URL
+      },
+    ],
+    tags: [
+      {
+        name: 'users',
+      },
+    ],
+  },
+  apis: ['./routes/*.ts', './controllers/*.ts'],
+};
 
+const swaggerSpecs = swaggerJSDoc(swaggerOptions);
 const app = express()
 
-app.use('/swagger', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/incident', incidentRout)
--- ims-server/src/routes/IncidentRout.ts
+++ ims-server/src/routes/IncidentRout.ts
@@ -1,12 +1,12 @@
-import express from 'express'
-import IncidenceController from '../controllers/IncidentControler'
+import express from 'express';
+import IncidenceController from '../controllers/IncidentControler';
 
-const router = express.Router()
-const incidenceController = new IncidenceController()
+const router = express.Router();
+const incidenceController = new IncidenceController();
 
-router.get('/', incidenceController.getAllIncidents)
-router.get('/:id', incidenceController.getIncidentById)
-router.post('/addIncident', incidenceController.addIncident)
-router.put('/updateIncidence/:id', incidenceController.updateIncidence)
+router.get('/', incidenceController.getAllIncidents);
+router.get('/:id', incidenceController.getIncidentById);
+router.post('/', incidenceController.addIncident);
+router.put('/:id', incidenceController.updateIncidence);
 
-export default router
+export default router;
