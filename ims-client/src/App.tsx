67f0bdeb2dfe999bcfb92020691c34f165e2cc61
אביטל פינץ <avital058pz@gmail.com>
2023-07-11 18:04:29 +0300

    [ims-21}- add story book to autocomplete

--- ims-client/src/App.tsx
+++ ims-client/src/App.tsx
@@ -10,7 +10,7 @@ import theme from './theme'
 import { Box } from '@mui/material'
 import { CustomFooter } from './components/Table/Footer'
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
-import AutocompleteTag from './components/autoCompleteTag/autoCompleteTag'
+ 
 //import { DatePickerComp } from './components/datePicker'
 import DemoComponent from './demoTest/DemoComponent'
 
@@ -33,15 +33,13 @@ import DemoComponent from './demoTest/DemoComponent'
 
 
 function App() {
-  const tagOptions = [{id:"a" ,name:'Tag1'}, {id:"b" ,name:'Tag2'}, {id:"c" ,name:'Tag3'}, {id:"d" ,name:'Tag4'}];
+  
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ display: 'flex' }}>
         <LeftDrawer />
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          {/* <Here put all the components /> */}
-           <AutocompleteTag tagOptions={tagOptions} ></AutocompleteTag>  
-          
+          {/* <Here put all the components /> */} 
         </Box>
       </Box>
     </ThemeProvider>
--- ims-client/src/components/autoCompleteTag/autoCompleteTag.tsx
+++ ims-client/src/components/autoCompleteTag/autoCompleteTag.tsx
@@ -1,66 +1,46 @@
 import React, { useState } from 'react';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
- 
-
-
- 
-
-interface Tag {
-  id: string,
-  name: string
+interface Tag{
+  id: string;
+  name: string;
 }
-interface getProps {
-  tagOptions: Tag[]
+interface AutocompleteTagProps{
+  tagOptions: Tag[];
 }
-const AutocompleteTag = ({ tagOptions }: getProps) => {
-  //const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
- 
+const AutocompleteTag = ({ tagOptions }: AutocompleteTagProps) => {
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
   return (
-    
-    //   value={selectedTags}
-    //   // onChange={(event, newValue) => {
-    //   //   setSelectedTags(newValue);
-    //   // }}
-    //   // renderTags={(values) =>
-    //   //   values.map((value) => (
-    //     //   <Chip
-    //     //     sx={{
-    //     //       border: "1px solid #2F854F", color: " #2F854F",
-    //     //       backgroundColor: "rgba(47, 133, 79, 0.10)", fontFamily: "Poppins", fontSize: "15px", fontStyle: "normal",
-    //     //       fontWeight: "100", lineHeight: "normal", href: "#basic-chip"
-    //     //     }}
-    //     //     key={value.id}
-    //     //     label={value.name}
-    //     //     onDelete={() => {
-    //     //       handleRemoveTag(value.id);
-    //     //     }}
-    //     //   />
-    //   //    ))
-    //   // }
-    
-    <Autocomplete multiple 
-      ChipProps={{  sx:{border: "1px solid #2F854F", color: " #2F854F",
-                       backgroundColor: "rgba(47, 133, 79, 0.10)", fontFamily: "Poppins", 
-                       fontSize: "15px", fontStyle: "normal",
-                       fontWeight: "100", lineHeight: "normal",
-                      }
-                 }}
+    <Autocomplete
+      ChipProps={{
+        clickable: true,
+        sx: {
+          border: '1px solid #2F854F',
+          color: '#2F854F',
+          backgroundColor: 'rgba(47, 133, 79, 0.10)',
+          fontFamily: 'Poppins',
+          fontSize: '15px',
+          fontStyle: 'normal',
+          fontWeight: '100',
+          lineHeight: 'normal',
+        },
+      }}
+      multiple
       options={tagOptions}
-      getOptionLabel={(option) => option.name}
       filterSelectedOptions
+      value={selectedTags}
+      getOptionLabel={(option) => option.name}
+      onChange={(event, newValue) => {
+        setSelectedTags(newValue);
+      }}
       renderInput={(params) => (
         <TextField
-           {...params}
-           placeholder="Write to add"
+          {...params}
+          placeholder="Write to add"
         />
       )}
       sx={{border:"1px solid #E1E1E1",borderRadius:"10px",background:"#FFF",width: "569px"}}
-      />
+    />
   );
 };
-
 export default AutocompleteTag;
- 
- 
- 
