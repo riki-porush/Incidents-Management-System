160c1c3c96d5c0fd09acff241964fab340c6d708
Batzion <Batziongreen3320@gmail.com>
2023-07-12 10:35:42 +0300

    DropDown[Ims-45]

--- ims-client/src/components/dropDoun/Types.tsx
+++ ims-client/src/components/dropDoun/Types.tsx
@@ -1,11 +1,17 @@
 
-interface Type {
+ export interface Type {
     value: string;
     label: string;
 };
   
-export const Types: Type[] = [
+export const Types2: Type[] = [
     { value: 'securing', label: 'securing' },
     { value: 'technical', label: 'technical' },
     { value: 'comment', label: 'comment' },
 ];
+export const Types1: Type[] = [
+    { value: 'securing', label: 'a' },
+    { value: 'technical', label: 'b' },
+    { value: 'comment', label: 'c' },
+];
+
