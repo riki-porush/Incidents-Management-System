24ff30c204255358f2575b90c59bc281a39fceda
Beily123 <bk0527680141@gmail.com>
2023-07-13 11:56:43 +0300

    update page name

--- ims-client/src/pages/incidents/incidentsPage.tsx
+++ ims-client/src/pages/incidents/incidentsPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import apiCalls from "../../service/apiCalls";
 import IncidentTable from "../../components/IncidentTable/IncidentTable";
-// import WidgetsStack from "../../components/Widget/WidgetsStack";
+import WidgetsStack from "../../components/Widget/WidgetsStack";
 
 const IncidentsPage = () => {
   const [incidents, setIncidents] = useState([]);
@@ -18,9 +18,8 @@ const IncidentsPage = () => {
     console.log("incident", incidents);
   }, [incidents]);
 
-  return (
-    <>
-      {/* <WidgetsStack /> */}
+  return ( <>
+      <WidgetsStack />
       <IncidentTable rows={incidents} />
     </>
   );
