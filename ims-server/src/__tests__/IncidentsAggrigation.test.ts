7266ab98eba5cf313228dc7783dba676c845a63c
Rivky Fisher <hand1b20@mbjcomp.org.il>
2023-07-16 10:30:54 +0300

    add aggregate test

--- ims-server/src/__tests__/IncidentsAggrigation.test.ts
+++ ims-server/src/__tests__/IncidentsAggrigation.test.ts
@@ -1,25 +1,47 @@
 import app from '../app';
 import supertest from 'supertest';
-import { json } from 'express';
 import IncidentModel from '../models/IncidentModel';
 
+  describe("aggregate incidents", () => {
+    describe("succeed", () => {
+      it("should return aggregated incident data", async () => {
+        const expectedResult = {
+          activeCount: 10,
+          averageCost: 500,
+          averageDurationHours: 5,
+        };
+        jest.spyOn(IncidentModel, 'aggregate').mockResolvedValueOnce([expectedResult]);
 
-describe("aggrigation", () => {
-    describe("get incidents aggrigations",()=>{
-        describe("succed",()=>{
-            it("should return data",async()=>{
-                const res=await supertest(app).get("/aggrigation/");
-                expect(res.status).toBe(200);
-            })
-        })
-        describe("error",()=>{
-            it("should return 404", async()=>{
-                jest.spyOn(IncidentModel, 'find').mockRejectedValueOnce(new Error());
-                const res=(await supertest(app).get("/aggrigation/"));
-                expect(res.status).toBe(404);
-            })
-        })
-    })
-})
+        const res = await supertest(app).get("/incident/aggregate");
+        expect(res.status).toBe(200);
+        expect(res.body).toEqual(expectedResult);
+      });
+    });
+
+    describe("no result", () => {
+      it("should return default aggregated incident data if no result", async () => {
+        jest.spyOn(IncidentModel, 'aggregate').mockResolvedValueOnce([]);
+
+        const res = await supertest(app).get("/incident/aggregate");
+        expect(res.status).toBe(200);
+        expect(res.body).toEqual({
+          activeCount: 0,
+          averageCost: 0,
+          averageDurationHours: 0,
+        });
+      });
+    });
+
+    describe("error", () => {
+      it("should return 404 if an error occurs during aggregation", async () => {
+        jest.spyOn(IncidentModel, 'aggregate').mockRejectedValueOnce(new Error());
+
+        const res = await supertest(app).get("/incident/aggregate");
+        expect(res.status).toBe(404);
+      });
+    });
+  });
+;
+
 
 
