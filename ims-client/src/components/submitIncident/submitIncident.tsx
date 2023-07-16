f35f41764926eb4e20d440fb14e14e7fdb0f2048
tamarmiller1 <tamarm@darkeychana.com>
2023-07-16 15:42:18 +0300

    [IMS-13] - open-> active

--- ims-client/src/components/submitIncident/submitIncident.tsx
+++ ims-client/src/components/submitIncident/submitIncident.tsx
@@ -11,7 +11,7 @@ export default async function submitIncident(prop:FormData) {
         //Remove the id
         id: "1",
         name: prop.name,
-        status:"Open",
+        status:"Active",
         description:prop.description,
         priority: prop.priority,
         type: prop.type,
