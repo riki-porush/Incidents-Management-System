24db4235563e1af5f80e7b0646b4a1c8d322e9ea
YaelKofman <yaelk1204@gmail.com>
2023-07-17 13:22:20 +0300

    change 2

--- ims-client/src/Stories/attachment.stories.tsx
+++ ims-client/src/Stories/attachment.stories.tsx
@@ -21,28 +21,28 @@ decorators: [
 ],
 } as Meta;
 
-const Template: Story<AttachmentProps> = (args) => <Attachment {...args} />;
+// const Template: Story<AttachmentProps> = (args) => <Attachment {...args} />;
 
 
-export const SingleImageSize = Template.bind({});
-SingleImageSize.args = {
-  attachmentFile: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB9hfMxrD1ywcTDkrqvYu2CPDaDifO3AtmLztsKh4ZqkvS1jZdEQ1DWupA9KJCrQ-wnZI&usqp=CAU"
-  ,
-  numberOfImages: 'singleImage',
-};
+// export const SingleImageSize = Template.bind({});
+// SingleImageSize.args = {
+//   attachmentFile: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB9hfMxrD1ywcTDkrqvYu2CPDaDifO3AtmLztsKh4ZqkvS1jZdEQ1DWupA9KJCrQ-wnZI&usqp=CAU"
+//   ,
+//   numberOfImages: 'singleImage',
+// };
 
-export const TwoImagesSize = Template.bind({});
-TwoImagesSize.args = {
-  attachmentFile: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB9hfMxrD1ywcTDkrqvYu2CPDaDifO3AtmLztsKh4ZqkvS1jZdEQ1DWupA9KJCrQ-wnZI&usqp=CAU"
-  ,
-  numberOfImages: 'twoImages',
-};
+// export const TwoImagesSize = Template.bind({});
+// TwoImagesSize.args = {
+//   attachmentFile: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB9hfMxrD1ywcTDkrqvYu2CPDaDifO3AtmLztsKh4ZqkvS1jZdEQ1DWupA9KJCrQ-wnZI&usqp=CAU"
+//   ,
+//   numberOfImages: 'twoImages',
+// };
 
-export const ThreeOrMoreImagesSize= Template.bind({});
-ThreeOrMoreImagesSize.args = {
-  attachmentFile: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB9hfMxrD1ywcTDkrqvYu2CPDaDifO3AtmLztsKh4ZqkvS1jZdEQ1DWupA9KJCrQ-wnZI&usqp=CAU"
-  ,
-  numberOfImages: 'threeAndMoreImages',
-};
+// export const ThreeOrMoreImagesSize= Template.bind({});
+// ThreeOrMoreImagesSize.args = {
+//   attachmentFile: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB9hfMxrD1ywcTDkrqvYu2CPDaDifO3AtmLztsKh4ZqkvS1jZdEQ1DWupA9KJCrQ-wnZI&usqp=CAU"
+//   ,
+//   numberOfImages: 'threeAndMoreImages',
+// };
 
 
--- ims-client/src/pages/timeLine/timeLineEvent/attachment/attachment.tsx
+++ ims-client/src/pages/timeLine/timeLineEvent/attachment/attachment.tsx
@@ -5,59 +5,21 @@ import { FileDownload } from '@mui/icons-material';
 
 export interface AttachmentProps {
   attachmentFile: string;
-  numberOfImages: string;
 }
-//דוגמה לזימון הקומפוננטה:
-{/* <Attachment attachmentFile="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB9hfMxrD1ywcTDkrqvYu2CPDaDifO3AtmLztsKh4ZqkvS1jZdEQ1DWupA9KJCrQ-wnZI&usqp=CAU"
-numberOfImages='singleImage'></Attachment> */}
 
-const Attachment: React.FC<AttachmentProps> = ({ attachmentFile, numberOfImages }) => {
 
-    const singleImageStyle: React.CSSProperties = {
-    height: '228px',
-    width: '500px',
-    position: 'relative',
-    borderRadius: '10px'
-  };
-  const twoImagesStyle: React.CSSProperties = {
-    height: '228px',
-    width: '352px',
-    position: 'relative',
-    borderRadius: '10px'
-  };
-  const threeAndMoreImagesStyle: React.CSSProperties = {
-    height: '228px',
-    width: '228px',
-    position: 'relative',
-    borderRadius: '10px'
-  };
-
-  const styleMap: { [key: string]: React.CSSProperties } = {
-    singleImage: {
-      ...singleImageStyle,
-    },
-    twoImages: {
-      ...twoImagesStyle,
-    },
-    threeAndMoreImages: {
-      ...threeAndMoreImagesStyle,
-    },
-  };
-
+const Attachment: React.FC<AttachmentProps> = ({ attachmentFile }) => {
   const iconStyle: React.CSSProperties = {
     position: 'absolute',
     zIndex: 1,
     bottom: 10,
     right: 10,
   };
-//mnn
+
   return (
     <div>
-      <div style={styleMap[numberOfImages]}>
-        <img src={attachmentFile} style={styleMap[numberOfImages]}  />
-        <FileDownload style={iconStyle} />
-        
-      </div>
+      <img src={attachmentFile} style={{ position: 'absolute', zIndex: '1 !important' }} />
+      <FileDownload style={{ position: 'absolute', zIndex: '3 !important' }} />
     </div>
   );
 };
